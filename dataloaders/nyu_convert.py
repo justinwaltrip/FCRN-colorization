12daf83f9614860ff181098cb0969c50f36bec73
Justin <jwaltri2@jhu.edu>
2023-05-04 15:43:54 +0000

    working

--- dataloaders/nyu_convert.py
+++ dataloaders/nyu_convert.py
@@ -1,5 +1,4 @@
 import numpy as np
-import scipy.io as sio
 import h5py
 import os
 
@@ -9,14 +8,7 @@ output_path = "data/nyu"  # where to put the resulting dataset
 
 train_path = os.path.join(output_path, "train/official/")
 val_path = os.path.join(output_path, "val/official/")
-
-splilts = sio.loadmat(data_path + "/splits.mat")
-
-train_idx = splilts["trainNdxs"]
-val_idx = splilts["testNdxs"]
-
-train_idx = np.array(train_idx)
-val_idx = np.array(val_idx)
+test_path = os.path.join(output_path, "test/official/")
 
 f = h5py.File(data_path + "/nyu_depth_v2_labeled.mat")
 images = f["images"]
@@ -27,12 +19,25 @@ images = np.array(images)
 depths = np.array(depths)
 labels = np.array(labels)
 
+# set random seed
+seed = 1
+np.random.seed(seed)
+
+# create 80-10-10 train-val-test split
+idx = np.arange(1, len(images) + 1)
+shuffle_idx = np.random.permutation(idx)
+train_idx = shuffle_idx[:int(0.8 * len(images))]
+val_idx = shuffle_idx[int(0.8 * len(images)):int(0.9 * len(images))]
+test_idx = shuffle_idx[int(0.9 * len(images)):]
 
 if not os.path.isdir(train_path):
     os.makedirs(train_path)
 
 if not os.path.isdir(val_path):
     os.makedirs(val_path)
+
+if not os.path.isdir(test_path):
+    os.makedirs(test_path)
 
 for idx in range(len(train_idx)):
     f_idx = "{0:0>5}".format(int(train_idx[idx]))
@@ -51,6 +56,16 @@ for idx in range(len(val_idx)):
 
     h5f["rgb"] = np.transpose(images[val_idx[idx] - 1][0], (0, 2, 1))
     h5f["depth"] = np.transpose(depths[val_idx[idx] - 1][0], (1, 0))
+
+    h5f.close()
+
+for idx in range(len(test_idx)):
+    f_idx = "{0:0>5}".format(int(test_idx[idx]))
+    print("test:", f_idx)
+    h5f = h5py.File(test_path + f_idx + ".h5", "w")
+
+    h5f["rgb"] = np.transpose(images[test_idx[idx] - 1][0], (0, 2, 1))
+    h5f["depth"] = np.transpose(depths[test_idx[idx] - 1][0], (1, 0))
 
     h5f.close()
 
